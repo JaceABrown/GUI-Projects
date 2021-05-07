0071ed93c36a66d21fd35378b5a21a7f3918e4be
JaceABrown <79872355+JaceABrown@users.noreply.github.com>
2021-05-07 12:41:10 -0700

    Add files via upload

---
+++ tkinter 1.py
@@ -0,0 +1,47 @@
+from tkinter import *
+
+top = Tk()
+songList = []
+
+def addTrack():
+    songList.append(E1.get())
+    E1.delete(0, END)
+
+def printList():
+    print(songList)
+
+def exportList():
+    with open("test.txt", "w") as f:
+        for item in songList:
+            f.write("%s\n" % item)
+
+def clearWindow():
+    for widget in top.winfo_childeren():
+        widget.destroy()
+
+#this is a label widget
+L1 = Label(top, text="ourTunes")
+L1.grid(column= 0, row= 1)
+
+#this is an entry widget (for text entry)
+E1 = Entry(top, bd = 5)
+E1.grid(column= 0, row = 2)
+
+
+B1 = Button(text = " + ", bg = "white", command= addTrack)
+B1.grid(column= 1, row = 2)
+
+B2 = Button(text = "playList", bg = "#d4850f", command = printList)
+B2.grid(column= 1, row = 1)
+
+B3 = Button(text="Export", bg= "#4940e6", command= exportList)
+B3.grid(column= 1, row = 3)
+
+
+Bclear = Button(text="clear", bg= "white" , command= clearWindow)
+Bclear.grid(column= 5, row = 20)
+                
+print(E1.get())
+
+top.mainloop()
+
