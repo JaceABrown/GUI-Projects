427bc0603e5554c4c7b5ba7cd4bb1e2df27cfe6b
JaceABrown <79872355+JaceABrown@users.noreply.github.com>
2021-05-11 12:37:17 -0700

    Add files via upload

--- tkinter 1.py
+++ tkinter 1.py
@@ -19,29 +19,64 @@ def clearWindow():
     for widget in top.winfo_childeren():
         widget.destroy()
 
-#this is a label widget
-L1 = Label(top, text="ourTunes")
-L1.grid(column= 0, row= 1)
+def mainMenu():
+    clearwindow()
+    LMain = Label(top, text="Block 5 GUI projects")
+    LMain.grid(column = 0, row=1)
+    
+    B1Main = Button(text = "week 1", bg = "white", command= week_1)
+    B1Main.grid(column= 1, row = 2)
 
-#this is an entry widget (for text entry)
-E1 = Entry(top, bd = 5)
-E1.grid(column= 0, row = 2)
+    B2Main = Button(text = "week 2", bg = "white", command= week_2)
+    B2Main.grid(column= 1, row = 3)
 
-
-B1 = Button(text = " + ", bg = "white", command= addTrack)
-B1.grid(column= 1, row = 2)
-
-B2 = Button(text = "playList", bg = "#d4850f", command = printList)
-B2.grid(column= 1, row = 1)
-
-B3 = Button(text="Export", bg= "#4940e6", command= exportList)
-B3.grid(column= 1, row = 3)
+    B3Main = Button(text = "week 3", bg = "white", command= week_3)
+    B3Main.grid(column= 1, row = 4)
 
 
-Bclear = Button(text="clear", bg= "white" , command= clearWindow)
-Bclear.grid(column= 5, row = 20)
-                
-print(E1.get())
+def week1():
+    clearwindow()
+    #this is a label widget
+    L1 = Label(top, text="ourTunes")
+    L1.grid(column= 0, row= 1)
+
+    #this is an entry widget (for text entry)
+    E1 = Entry(top, bd = 5)
+    E1.grid(column= 0, row = 2)
+
+
+    B1 = Button(text = " + ", bg = "white", command= addTrack)
+    B1.grid(column= 1, row = 2)
+
+    B2 = Button(text = "playList", bg = "#d4850f", command = printList)
+    B2.grid(column= 1, row = 1)
+
+    B3 = Button(text="Export", bg= "#4940e6", command= exportList)
+    B3.grid(column= 1, row = 3)
+
+def week2():
+    clearWindow()
+
+    L1W2 = Label(top, text="Dice Roller App")
+    L1W2.grid(column= 0, row = 2)
+    
+    L2W2 = Label(top, text="# of Sides")
+    L1W2.grid(column= 1, row = 2)
+    
+    L3W2 = Label(top, text="# of Rolls")
+    L1W2.grid(column= 1, row = 2)
+    
+    E1W2 = Entry(top, bd = 5)
+    
+    E1W2 = Entry(top,bd = 5)
+    
+    B1W2 = Button(text= "Roll 'em!", bd = "yellow)
+
+    
+    Bclear = Button(text="clear", bg= "white" , command= clearWindow)
+    Bclear.grid(column= 5, row = 20)
+                    
+    print(E1.get())
 
 top.mainloop()
 
